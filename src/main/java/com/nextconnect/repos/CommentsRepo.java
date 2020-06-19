534747ac30abe523420d6c5726ffec46047aa407
atadiboy <atadiboy@ATADIBOY-IN.oradev.oraclecorp.com>
2020-06-20 00:15:45 +0530

    remove unnecessary files

--- src/main/java/com/nextconnect/repos/CommentsRepo.java
+++
@@ -1,9 +0,0 @@
-package com.nextconnect.repos;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.nextconnect.dto.Comments;
-
-public interface CommentsRepo extends CrudRepository<Comments, Integer>{
-
-}
