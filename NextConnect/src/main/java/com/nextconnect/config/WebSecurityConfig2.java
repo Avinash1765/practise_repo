0e0010107ac13584ece08a44cbea3bcd4dc547e7
atadiboy <atadiboy@ATADIBOY-IN.oradev.oraclecorp.com>
2020-07-02 22:37:11 +0530

    adding basic auth to apis

--- NextConnect/src/main/java/com/nextconnect/config/WebSecurityConfig2.java
+++ NextConnect/src/main/java/com/nextconnect/config/WebSecurityConfig2.java
@@ -2,20 +2,32 @@ package com.nextconnect.config;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableWebSecurity
+@Configuration
 public class WebSecurityConfig2 extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private AuthenticationEntryPoint authEntryPoint;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.cors().and().authorizeRequests().antMatchers("/**").permitAll().and().csrf().disable();
+		http.cors().and().authorizeRequests().
+		antMatchers("/**").authenticated().and().httpBasic()
+		  .authenticationEntryPoint(authEntryPoint)
+		  .and().csrf().disable();
+		
 	}
 
 	@Bean
@@ -27,5 +39,17 @@ public class WebSecurityConfig2 extends WebSecurityConfigurerAdapter {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
 		return source;
+	}
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+        .withUser("user1").password(bCryptPasswordEncoder().encode("user1Pass"))
+        .authorities("ROLE_USER");
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+	    return new BCryptPasswordEncoder();
 	}
 }
